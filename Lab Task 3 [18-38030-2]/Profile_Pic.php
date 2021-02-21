32174c93291574b63610f1309c2513af72942afe
Abu Bakar Siddiq <75138728+abanik310@users.noreply.github.com>
2021-02-22 01:00:40 +0600

    Add files via upload

---
+++ Lab Task 3 [18-38030-2]/Profile_Pic.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="PicUpload.php" method="post" enctype="multipart/form-data">
+	<img src="17241-200.png" alt="Profile Pic" width="100" height="120"><br>
+ 
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Submit" name="submit">
+</form>
+
+</body>
+</html>
