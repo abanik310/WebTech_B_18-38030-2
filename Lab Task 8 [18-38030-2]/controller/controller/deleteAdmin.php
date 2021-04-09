c39e3bb55a6f70be03b2e80f0415dcaad2486466
Abu Bakar Siddiq <75138728+abanik310@users.noreply.github.com>
2021-04-10 04:41:54 +0600

    Delete deleteAdmin.php

--- Lab Task 8 [18-38030-2]/controller/controller/deleteAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../model/model.php';
-if (deleteAdmin($_GET['o-id'])) {
-    header('Location: ../view/viewAllAdmins.php');
-    echo "Admin deleted";
-} else {
-    echo 'error';
-}
