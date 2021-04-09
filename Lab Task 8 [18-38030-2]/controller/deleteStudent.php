02e4050f422fc8b25f8adc8eb86aa260f246fa3d
Abu Bakar Siddiq <75138728+abanik310@users.noreply.github.com>
2021-04-10 04:39:01 +0600

    Delete deleteStudent.php

--- Lab Task 8 [18-38030-2]/controller/deleteStudent.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../model/model.php';
-if (deleteStudent($_GET['o-id'])) {
-    header('Location: ../view/viewAllStudents.php');
-    echo "Student deleted";
-} else {
-    echo 'error';
-}
