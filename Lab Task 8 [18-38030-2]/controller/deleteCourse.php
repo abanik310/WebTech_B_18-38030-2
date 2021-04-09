1af18b880374ce1858d4b284fb0b81f2d56ebdc4
Abu Bakar Siddiq <75138728+abanik310@users.noreply.github.com>
2021-04-10 04:38:56 +0600

    Delete deleteCourse.php

--- Lab Task 8 [18-38030-2]/controller/deleteCourse.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../model/model.php';
-if (deleteCourse($_GET['c-id'])) {
-    header('Location: ../view/viewAllCourse.php');
-    echo "Course deleted";
-} else {
-    echo 'error';
-}
